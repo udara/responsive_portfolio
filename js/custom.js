3b4e8b22f889c7999a64031a65262a3e1659e9d2
Udara ranasinghe <indi79@hotmail.com>
2020-02-25 22:42:44 +1030

    minor chnages

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,19 @@
 $(window).on('load resize', function () {
-    resizeGallery();
+  if ($(window).width() >= 992)
+  {  
+  resizeGallery();
+  }
+  else
+  {
+    $('.project-sub').css({'height':'auto'});
+  }
   });
 
   function resizeGallery()
   {
   var projectMainWidth = $('.project-main').width();
   var projectSubWidth = $('.project-sub').width();
+  projectSubWidth = projectSubWidth + 16;
   $('.project-main').css({'min-height':projectMainWidth+'px'});
   $('.project-sub').css({'min-height':projectSubWidth+'px'});
   }
