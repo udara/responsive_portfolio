a374e151d4cf64f2f41096e514014021bcc44fc9
Udara ranasinghe <indi79@hotmail.com>
2020-02-26 14:49:54 +1030

    Adding code for stickey footer

--- js/custom.js
+++ js/custom.js
@@ -7,13 +7,30 @@ $(window).on('load resize', function () {
   {
     $('.project-sub').css({'height':'auto'});
   }
-  });
+});
 
-  function resizeGallery()
-  {
+/*Keep divs on Project Gallery Square at all times width = height*/
+function resizeGallery(){
   var projectMainWidth = $('.project-main').width();
   var projectSubWidth = $('.project-sub').width();
   projectSubWidth = projectSubWidth + 16;
   $('.project-main').css({'min-height':projectMainWidth+'px'});
   $('.project-sub').css({'min-height':projectSubWidth+'px'});
-  }
+}
+
+/* Show || Hide sticky footer when scrolled to wondow.height = var footerOffHeight*/
+var windowHeight = $(window).height();
+var footerOffHeight = windowHeight * .95;
+var footerHeight = 90; 
+var addextraheight = $('body').height() + footerHeight; 
+
+$(window).on('scroll', function (){
+    if(window.pageYOffset > footerOffHeight)
+    {
+      $('.footer').css('height','90px');
+      $('body').css({'height':addextraheight+'px'});
+    }
+    else{
+      $('.footer').css('height','0');
+    }
+});
